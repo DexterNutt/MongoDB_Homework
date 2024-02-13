cab13e36f8a74aa9f807cc6cd6e8cdb9b13ba50a
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-02-13 20:09:46 +0100

    Add database to variable and use in the URL

--- HW/database/database.js
+++ HW/database/database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-// const db = 'MongoDB_Homework';
+const db = 'MongoDB_Homework';
 
 exports.connectToDatabase = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://dexternutt:IQmZkcUJTJMZ56Cm@cluster0.phtr5jb.mongodb.net/MongoDB_Homework?retryWrites=true&w=majority`
+      `mongodb+srv://dexternutt:IQmZkcUJTJMZ56Cm@cluster0.phtr5jb.mongodb.net/${db}?retryWrites=true&w=majority`
     );
     console.log('Connected to database!');
   } catch (err) {
