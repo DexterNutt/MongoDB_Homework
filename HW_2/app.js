762e87657b060d440efd639b15e03f2a86ce819b
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-02-20 19:56:55 +0100

    Change http methods in delete

--- HW_2/app.js
+++ HW_2/app.js
@@ -40,8 +40,8 @@ app.post('/api/v1/login', auth.login);
 app.get('/advertisements', adverts.getAllAdverts);
 app.get('/advertisement/:id', adverts.getAdvert);
 app.post('/api/v1/advertisements', adverts.createAdvert);
-app.get('/api/v1/advertisements/delete/:id', adverts.deleteAdvert);
-app.patch('/api/v1/advertisements/update/:id', adverts.updateAdvert);
+app.delete('/api/v1/advertisements/:id', adverts.deleteAdvert);
+app.patch('/api/v1/advertisements/:id', adverts.updateAdvert);
 
 app.listen(process.env.PORT, error => {
   if (error) console.log('Could not initiate server');
